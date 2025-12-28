f4ed615fcf46322d0ce19e2f205d251ed54a93b3
joaoV <joaovtgb@gmail.com>
2025-12-28 17:56:57 -0300

    feat: adiciona prioridade às tarefas (mudança de escopo)

--- src/main/java/br/com/unifecaf/Task.java
+++ src/main/java/br/com/unifecaf/Task.java
@@ -5,11 +5,13 @@ public class Task {
     private int id;
     private String titulo;
     private String descricao;
+    private String prioridade;
 
     public Task(int id, String titulo, String descricao) {
         this.id = id;
         this.titulo = titulo;
         this.descricao = descricao;
+        this.prioridade = "MEDIA";
     }
 
     public int getId() {
@@ -31,4 +33,9 @@ public class Task {
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+
+    public String getPrioridade() {
+        return prioridade;
+    }
+
 }
